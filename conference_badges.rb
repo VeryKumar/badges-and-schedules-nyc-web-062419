b4be91435402768ae62aaeac93375ac6343e316c
Varun <uberkumar@gmail.com>
2019-06-24 21:20:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names_arr = []
+  for i in names
+    names_arr << "Hello, my name is #{i}."
+  end
+  return names_arr
+end
+
+def assign_rooms(speakers)
+  rooms_arr = []
+  room = 1
+  for i in speakers
+    rooms_arr << "Hello, #{i}! You'll be assigned to room #{room}!"
+    room += 1 
+  end
+  return rooms_arr
+end
+
+def printer(names)
+  batch_badge_creator(names)
+  assign_rooms(names)
+  for i in range 1..assign_rooms.length
+    puts names_arr[i] + rooms_arr[i]
+  end
+end
+  
